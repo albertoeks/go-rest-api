a69a36d618b8bd4731e5eadd8a702d3fd7011182
albertoeks <eks.alberto@gmail.com>
2020-05-07 22:50:16 +1000

    Add /tasks endpoint

--- main/main.go
+++ main/main.go
@@ -1,10 +1,18 @@
 package main
 
 import (
+	"encoding/json"
 	"fmt"
 	"log"
 	"net/http"
 )
+
+type Task struct {
+	Title string `json:"Title"`
+	Content string `json:"content"`
+}
+
+var Tasks []Task
 
 func homePage(w http.ResponseWriter, r *http.Request){
 	fmt.Fprintf(w, "Welcome to the Go HomePage!")
@@ -13,9 +21,20 @@ func homePage(w http.ResponseWriter, r *http.Request){
 
 func handleRequests() {
 	http.HandleFunc("/", homePage)
+	http.HandleFunc("/tasks", getAllTasks)
 	log.Fatal(http.ListenAndServe(":10000", nil))
 }
 
+func getAllTasks(w http.ResponseWriter, r *http.Request){
+	fmt.Println("Endpoint Hit: getAllTasks")
+	json.NewEncoder(w).Encode(Tasks)
+}
+
 func main() {
+	Tasks = []Task{
+		Task{Title: "First task", Content: "First task content"},
+		Task{Title: "Second task", Content: "Second task content"},
+	}
+
 	handleRequests()
 }
